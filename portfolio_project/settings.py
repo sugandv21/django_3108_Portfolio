5f06d280753b58f6ae83a044b57375a8d9877698
sugandv21 <sdvsuganya@gmail.com>
2025-08-31 14:11:15 +0530

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -21,13 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")  
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+DEBUG = "RENDER" not in os.environ
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -128,3 +124,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
